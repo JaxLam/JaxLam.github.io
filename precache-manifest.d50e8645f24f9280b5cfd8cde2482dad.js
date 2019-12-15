4230f5cb6076c4e03f1778bef45558bbbe3b9ddf
JaxLam <jaxkinglam@gmail.com>
2019-12-15 12:34:38 -0800

    new

---
+++ precache-manifest.d50e8645f24f9280b5cfd8cde2482dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe452f33658c2da44bb9e0b81a5cea5",
+    "url": "/jaxlin12.github.io/index.html"
+  },
+  {
+    "revision": "fe07bae8541452a94a09",
+    "url": "/jaxlin12.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3ad6d12d46f234df84c1",
+    "url": "/jaxlin12.github.io/static/js/2.497a43ea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/jaxlin12.github.io/static/js/2.497a43ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe07bae8541452a94a09",
+    "url": "/jaxlin12.github.io/static/js/main.9539eba0.chunk.js"
+  },
+  {
+    "revision": "ddf4797dee0b0e8d764f",
+    "url": "/jaxlin12.github.io/static/js/runtime-main.499edbb4.js"
+  }
+]);
